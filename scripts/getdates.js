e4646e6baff2540b7b8a84a97b602d00a5b6873c
Teejman17 <tmccoy@byupathway.edu>
2025-05-14 23:48:54 -0600

    wdd 131 update

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const CurrentYear = new Date().getFullYear();
+
+const yearSpan = document.querySelector('footer #currentyear');
+
+const lastModifiedParagraph = document.querySelector('footer #lastModified')
+// alert(document.lastModified);
+
