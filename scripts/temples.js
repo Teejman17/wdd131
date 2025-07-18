8005192e9a0863078acceba31ca4dac450dae00e
Teejman17 <teejman17@gmail.com>
2025-07-17 18:19:06 -0600

    W02 Temples updates

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,14 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+document.addEventListener("DOMContentLoaded", () => {
+	const hamButton = document.querySelector('#menu');
+	const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+	hamButton.addEventListener('click', () => {
+		navigation.classList.toggle('open');
+		hamButton.classList.toggle('open');
+	});
 });
 
-const currentDate = new Date();
+const currentDate = new Date.getFullYear();
+document.getElementById('copyrightYear').textContent = currentYear;
+const lastModified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = Date;
