1f6215bc650c4693b89727381e8e633de1b2d9eb
Teejman17 <teejman17@gmail.com>
2025-08-11 03:51:00 -0600

    Project updates 2.0

---
+++ project/scripts/purchase.js
@@ -0,0 +1,20 @@
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
+
+const currentYear = new Date().getFullYear();
+const copyrightYearElement = document.getElementById('copyrightYear');
+if (copyrightYearElement) {
+    copyrightYearElement.textContent = currentYear;
+}
+const lastModifiedElement = document.getElementById('lastModified');
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = new Date(document.lastModified).toLocaleString('en-US', {
+        month: '2-digit', day: '2-digit', year: 'numeric',
+        hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false
+    })
+};
