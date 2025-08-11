b07cc333d22c2ed23db5d299c4e5f13853f4b2ae
Teejman17 <teejman17@gmail.com>
2025-08-11 02:10:49 -0600

    Project updates

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,3 +1,11 @@
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
+
 const currentYear = new Date().getFullYear();
 const copyrightYearElement = document.getElementById('copyrightYear');
 if (copyrightYearElement) {
